14372a6ac65a2f6df2428c0e3b775b06b28f1cc8
PranayChander <chander.pranay@gmail.com>
2021-02-07 13:38:58 +0530

    Programmatic navigation to Login VC

--- MazeMovies/LoginViewController.swift
+++ MazeMovies/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  MazeMovies
 //
 //  Created by pranay chander on 07/02/21.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
 }
-
--- MazeMovies/SceneDelegate.swift
+++ MazeMovies/SceneDelegate.swift
@@ -17,6 +17,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = scene as? UIWindowScene else { return }
+        self.window = UIWindow(windowScene: windowScene)
+        self.window?.rootViewController = LoginViewController()
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
